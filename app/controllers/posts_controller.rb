2a7241e98f4bb2726685be54a85a01c3bfcb4ff0
Adam <ajwieme@gmail.com>
2015-08-14 14:54:22 -0500

    fixing search query

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,14 +15,16 @@ class PostsController < ApplicationController
     @posts_and_scores_hash = Hash.new
     @posts_and_tags_hash = Hash.new
     search_tokens = params[:search].split(" ")
-    @tags = Tag.joins(:posts_tags).where(tag: search_tokens).order("posts_tags.term_score DESC").uniq
-    @tags.each do |tag|
-      @posts_tags = @posts_tags ? @posts_tags + PostsTag.where(tag_id: tag.id) : PostsTag.where(tag_id: tag.id)
+    @tags = Tag.where(tag: search_tokens).uniq
+    if @tags.present?
+      @tags.each do |tag|
+        @posts_tags = @posts_tags ? @posts_tags + PostsTag.where(tag_id: tag.id) : PostsTag.where(tag_id: tag.id)
+      end
+      @posts_tags.each do |posts_tag|
+        @posts_and_scores_hash[posts_tag.post] = posts_tag.post.search_score(@posts_tags) #[post => score]
+        @posts_and_tags_hash[posts_tag.post] = posts_tag.post.tags_and_scores_hash(@posts_tags) #[post => [tag => score]]
+      end
+      @posts_and_scores_hash = @posts_and_scores_hash.sort_by {|post, score| score}
     end
-    @posts_tags.each do |posts_tag|
-      @posts_and_scores_hash[posts_tag.post] = posts_tag.post.search_score(@posts_tags) #[post => score]
-      @posts_and_tags_hash[posts_tag.post] = posts_tag.post.tags_and_scores_hash(@posts_tags) #[post => [tag => score]]
-    end
-    @posts_and_scores_hash = @posts_and_scores_hash.sort_by {|post, score| score}
   end
 end
